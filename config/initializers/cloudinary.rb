0c925072743f223faae59aaa6f3e20fe1b1232af
Clyde Thomson <thomson.clyde@outlook.com>
2018-11-14 21:13:07 +0100

    Cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'hfqbomylo' 
+    config.api_key = '183281716346692' 
+    config.api_secret = 'uCgutFjCvq2O1IRqFwxWvhI1AFk' 
+    config.secure = true 
+    config.cdn_subdomain = true 
+end
