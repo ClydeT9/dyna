6a6f79d35ffd7b866ba25edcf382e747fd10ae31
Clyde Thomson <thomson.clyde@outlook.com>
2018-11-15 12:19:46 +0100

    enfin

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = "hoxr8ugj3"
-    config.api_key = "975985594182779"
-    config.api_secret = "D5kXxlpctIaQdpqLn50lqepzsM4"
-    config.secure = true
-    config.cdn_subdomain = true
-  end
