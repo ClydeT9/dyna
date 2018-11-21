6124e9d72d57e764a991145d7bf22780411307fb
Clyde Thomson <thomson.clyde@outlook.com>
2018-11-21 18:45:48 +0100

    new test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    layout proc{|c| c.request.xhr? ? false : "application" } #permet d'avoir un render différent avec js et html (render false pour les modal)
 end
